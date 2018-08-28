0f637c0d1a3bec8e73661b3ca4a1f8ffd0207062
Mike Choi <mike.choi@live.ca>
2018-08-27 21:16:10 -0700

    Added Javascript to end game when max score reached, and ability to modify or reset the game

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-var player1Score = document.querySelector("h1#score #player1");
-var player2Score = document.querySelector("h1#score #player2");
+var p1Score     = 0;
+var p1Display   = document.querySelector("#player1");
+var p1Button    = document.querySelector("#P1Button");
 
+var p2Score     = 0;
+var p2Display   = document.querySelector("#player2");
+var p2Button    = document.querySelector("#P2Button");
 
-player1Score.style.color = "red";
+var resetButton = document.querySelector("#resetButton");
+
+var gameOver    = false;
+var winningVal  = null;
+var scoreTo     = document.querySelector("#scoreTo");
+var maxScore    = document.querySelector("#maxScore");
+
+p1Button.addEventListener("click", () => {
+    if(!gameOver)
+    {
+        p1Display.textContent = p1Score+=1;
+        if(p1Score >= winningVal){
+            gameOver = true;
+            p1Display.classList.add("winner");
+        }
+    }
+});
+
+p2Button.addEventListener("click", () => {
+    if(!gameOver)
+    {
+        p2Display.textContent = p2Score+=1;
+        if(p2Score >= winningVal){
+            gameOver = true;
+            p2Display.classList.add("winner");
+        }
+    }
+});
+
+resetButton.addEventListener("click",  reset);
+
+maxScore.addEventListener("change", function() {
+    scoreTo.textContent = this.value;
+    winningVal = Number(this.value);
+    reset();
+});
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
